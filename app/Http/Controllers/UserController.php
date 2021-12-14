d86e98a6d94ff6f28cce24c5e9ee42144dbcbd95
Rafaella Bergamo <rafaellabergamo3@gmail.com>
2021-12-14 20:52:30 -0300

    Atualizando as rotas e removendo a view index pois não vai ser usada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,18 +35,5 @@ class UserController extends BaseController
     {
         return $repository->findAll();
     }
-
-    private function validate($request) {
-
-        $rules = [
-            'name' => 'required|min:10', 
-            'userName' => 'required|min:5', 
-            'zipCode' => 'required|min:8', 
-            'email' => 'required|min:10',
-            'password' => 'required|min:8'
-        ];
-
-        $request->validate($rules);
-    }
     
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/users', 'App\Http\Controllers\UserController@search')->name('search');
-Route::post('/users', 'App\Http\Controllers\UserController@register')->name('register');
+Route::get('/users', 'App\Http\Controllers\UserController@search');
+Route::post('/users', 'App\Http\Controllers\UserController@register');
