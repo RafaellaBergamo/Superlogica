fe027049ed88986c7a246f721017864869fa334d
Rafaella <rafaella.bergamo@plasutil.com.br>
2021-12-15 11:19:30 -0300

    Criando busca de usuário por id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,7 @@ class UserController extends BaseController
         return $repository->findAll();
     }
 
-    public function searchOne($id, UserRepositoryInterface $repository) 
+    public function searchOne(UserRepositoryInterface $repository) 
     {
         return $repository->findOne($id);
     }
