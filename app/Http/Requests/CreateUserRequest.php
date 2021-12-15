0e1dfc1dece799c978aaf1981a8f2268338b7cfa
Rafaella <rafaella.bergamo@plasutil.com.br>
2021-12-15 10:25:53 -0300

    última validação de dados

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -26,7 +26,7 @@ class CreateUserRequest extends FormRequest
         return [
             'name' => 'bail|required|regex:/^[a-zA-Z]+\s+[a-zA-Z]{0,}$/',
             'userName' => 'bail|required|min:5',
-            'zipCode' => 'bail|required|integer|regex:/^\d{8}$/',
+            'zipCode' => 'bail|required|regex:/^\d{8}$/',
             'email' => 'bail|required|unique:App\Models\UserModel|email:rfc,dns', 
             'password' => 'bail|required|regex:/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/'
         ];
