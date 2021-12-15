9c08dfc25c5a104b54d95ebcfed5bdd5847350e3
Rafaella <rafaella.bergamo@plasutil.com.br>
2021-12-15 10:33:04 -0300

    Melhorando a tabela

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,8 +21,6 @@ class CreateUsersTable extends Migration
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
